b4f8c0717478d7c4d12711355530457919ddac6e
toanpt301 <phamthanhtoan301bt@gmail.com>
2024-07-18 11:37:43 +0700

    Nhấn vào button ở HomeFragment sẽ chuyển đến các fragment tương ứng

--- app/src/main/java/com/example/duanmau/fragment/HomeFragment.java
+++ app/src/main/java/com/example/duanmau/fragment/HomeFragment.java
@@ -4,18 +4,63 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.duanmau.R;
 
 public class HomeFragment extends Fragment {
+    Button btnQlLoaiSach, btnQlPhieuMuon, btnThanhVien, btnThongKe;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //anh xa
+        btnQlLoaiSach = view.findViewById(R.id.btnQlLoaiSach);
+        btnQlPhieuMuon = view.findViewById(R.id.btnQlPhieuMuon);
+        btnThanhVien = view.findViewById(R.id.btnThanhVien);
+        btnThongKe = view.findViewById(R.id.btnThongKe);
+
+        btnQlPhieuMuon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = null;
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.frameLayout, new QLPhieuMuonFragment()).commit();
+            }
+        });
+
+        btnQlLoaiSach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = null;
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.frameLayout, new QLTheLoaiSachFragment()).commit();
+            }
+        });
+
+        btnThanhVien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = null;
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.frameLayout, new QLThanhVienFragment()).commit();
+            }
+        });
+
+        btnThongKe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = null;
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.frameLayout, new ThongKeFragment()).commit();
+            }
+        });
 
         return view;
     }
